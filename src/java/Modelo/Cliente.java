cadec4bb8afba571fd8b4ab9a60724ec62287f5c
dreidka <asanchezrincon1207@gmail.com>
2024-02-21 02:31:16 -0500

    probando jsp

---
+++ src/java/Modelo/Cliente.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author asanc
+ */
+public class Cliente {
+    String name;
+    String email;
+    int age;
+
+    public Cliente() {
+    }
+
+    public Cliente(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+}
---
+++ web/index.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : index
+    Created on : 21 Feb 2024, 02:19:09
+    Author     : asanc
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <form action="Registro" method="Post">
+            <input type="text" name="txtName">
+            <input type="email" name="txtEmail">
+            <input type="age" name="txtAge">
+            <input type="submit" name="btnSubmit">
+        </form>
+    </body>
+</html>
